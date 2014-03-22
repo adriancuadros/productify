97ea2571269f94155f5b89993c46f19904c01edd
Adrian Cuadros <adrian@innku.com>
2014-03-22 11:23:52 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Productify::Application.config.secret_key_base = '9dae5d4bb3e4c22d1ec748c4ebaf792201ad048c2db8dec16cb40b263af46adee156768609db80adfbc4864d144f5128b0cc0e3ef7bc8981ba01f7b7cdbc0951'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Productify::Application.config.session_store :cookie_store, key: '_productify_session'
